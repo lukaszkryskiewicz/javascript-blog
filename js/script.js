4c9dcc326ebaeadbae505ed1827b97f4a8ebd4a4
Lukasz Kryskiewicz <lukasz.kryskiewicz@gmail.com>
2022-10-21 21:40:03 +0200

    Add tag list on the right side as object to count tags

--- js/script.js
+++ js/script.js
@@ -84,8 +84,8 @@
   generateTitleLinks();
 
   function generateTags() {
-    /* [NEW] create a new variable allTags with an empty array */
-    let allTags = [];
+    /* [NEW] create a new variable allTags with an empty object */
+    let allTags = {};
     /* find all articles */
     const articles = document.querySelectorAll(optArticleSelector);
     /* START LOOP: for every article: */
@@ -105,9 +105,11 @@
         /* add generated code to html variable */
         html = html + tagHtml;
         /* [NEW] check if this link is NOT already in allTags */
-        if (allTags.indexOf(tagHtml) == -1) {
-          /* [NEW] add generated code to allTags array */
-          allTags.push(tagHtml);
+        if (!allTags[tag]) {
+          /* [NEW] add generated code to allTags object */
+          allTags[tag] = 1;
+        } else {
+          allTags[tag]++;
         }
         /* END LOOP: for each tag */
       }
@@ -117,8 +119,18 @@
     }
     /* [NEW] find list of tags in right column */
     let tagList = document.querySelector(optTagsListSelector);
-    /* [NEW] add html from allTags to tagList */
-    tagList.innerHTML = allTags.join(' ');
+    /* [NEW] create variable for all links HTML code */
+    let allTagsHTML = '';
+
+    /* [NEW] START LOOP: for each tag in allTags: */
+    for (let tag in allTags) {
+      /* [NEW] generate code of a link and add it to allTagsHTML */
+      allTagsHTML += '<li><a href="#tag-' + tag + '">' + tag + ' (' + allTags[tag] + ')</a></li>';
+    }
+    /* [NEW] END LOOP: for each tag in allTags: */
+
+    /*[NEW] add HTML from allTagsHTML to tagList */
+    tagList.innerHTML = allTagsHTML;
   }
 
   generateTags();
